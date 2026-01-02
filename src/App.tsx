e11f1fb74a875b38b90a2265aea5361f0cce391f
Satyam <142300565+itzsam-lol@users.noreply.github.com>
2026-01-02 21:29:01 +0530

    📦 Add react-helmet, update App routes, add robots.txt & sitemap

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Events from "./pages/Events";
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
@@ -27,6 +28,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/events" element={<Events />} />
           <Route path="/join-us" element={<JoinUs />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/newsletter" element={<Newsletter />} />
